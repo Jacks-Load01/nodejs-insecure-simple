62d24b2a4432e9a44f4f00a96f0ba3c20dd0f61d
Ian Spiro <ispiro@codiscope.com>
2016-05-02 16:08:03 -0400

    reworked route function

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var http = require('http');
-http.createServer(function (request, response) {
-	if (request.method === 'POST') {
-		var data = '';
-		request.addListener('data', function(chunk) {
-			console.log("Got some data! => " + chunk);
-			data += chunk;
-		});
-		request.addListener('end', function() {
-			console.log("...and end.")
-			try {
-				eval("(" + data + ")");
-			}
-			catch (exception) {}
-		});
-	}
-}).listen(process.env.PORT || 3000);
+function f1(request, response) {
+  if (request.method === 'POST') {
+    var data = '';
+    request.addListener('data', function(chunk) {
+      console.log("Got some data! => " + chunk);
+      data += chunk;
+    });
+    request.addListener('end', function() {
+      console.log("...and end.")
+      try {
+        eval("(" + data + ")");
+      }
+      catch (exception) {}
+    });
+  }
+http.createServer(f1);
 console.log('The server is on...'); 
